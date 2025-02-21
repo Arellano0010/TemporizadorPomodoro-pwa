a2dd2c399a01bdafa4d61203e68d88b65ea842ba
Arellano0010 <joelarellano0020@outlook.com>
2025-02-20 20:57:37 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,15 @@
-const CACHE_NAME = "pomodoro-cache-v1";
-const urlsToCache = [
-    "index.html",
-    "manifest.json",
-    "icon-192.png",
-    "icon-512.png"
-];
-
-// Instalación del Service Worker
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(cache => cache.addAll(urlsToCache))
+        caches.open("static-cache").then((cache) => {
+            return cache.addAll(["./", "./index.html", "./styles.css", "./script.js", "./manifest.json", "./icon-192x192.png", "./icon-512x512.png"]);
+        })
     );
 });
 
-// Interceptar peticiones y responder con cache
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
     event.respondWith(
-        caches.match(event.request)
-        .then(response => response || fetch(event.request))
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
     );
 });
